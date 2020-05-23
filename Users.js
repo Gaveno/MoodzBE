aac451222aff86109e68ce00dac091221bdd56cd
Gavin <gavin.monkeystudios@gmail.com>
2020-05-23 10:49:40 -0600

    Initial Project Commit

---
+++ Users.js
@@ -0,0 +1,47 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.DB, { useNewUrlParser: true } );
+mongoose.set('useCreateIndex', true);
+
+
+var UserSchema = new Schema({
+    username: { type: String, required: true },
+    lastName: { type: String },
+    firstName: { type: String },
+    password: { type: String, required: true }, // Stored as a hashed copy
+    imgProfile: { data: Buffer, contentType: String}, // profile photo
+    isAdmin: { type: Boolean },
+    disabled: { type: Boolean, required: false }
+});
+
+// hash the password before the user is saved
+UserSchema.pre('save', function(next) {
+    var user = this;
+
+    // hash the password only if the password has been changed or user is new
+    if (!user.isModified('password')) return next();
+
+    // generate the hash
+    bcrypt.hash(user.password, null, null, function(err, hash) {
+        if (err) return next(err);
+
+        // change the password to the hashed version
+        user.password = hash;
+        next();
+    });
+});
+
+UserSchema.methods.comparePassword = function(password, callback) {
+    var user = this;
+
+    bcrypt.compare(password, user.password, function(err, isMatch) {
+        callback(isMatch) ;
+    });
+};
+
+// return the model
+module.exports = mongoose.model('User', UserSchema);
